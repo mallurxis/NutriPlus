08be6ad6ec8e72893a45ef8292462ac471c8a7bf
JonathanAlves05 <jarprodrigues05@gmail.com>
2024-10-01 22:12:34 -0300

    Incluindo modelo historico

---
+++ app/src/main/java/br/com/etecia/nutriapp/HistoricoAdapter.java
@@ -0,0 +1,12 @@
+package br.com.etecia.nutriapp;
+
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoricoAdapter extends RecyclerView.Adapter<HistoricoAdapter> {
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView txtPratoHistorico;
+
+    }
+}
