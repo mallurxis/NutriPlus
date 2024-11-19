27a10a7c68af12552e609da68f9e37cb164223c0
arthurnxtto <arthurvnetto8@outlook.com>
2024-11-19 18:34:43 -0300

    conectando Cadastro e Login pelo firebase

--- app/src/main/java/br/com/etecia/nutriapp/activity/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/nutriapp/activity/CadastroActivity.java
@@ -2,19 +2,34 @@ package br.com.etecia.nutriapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import br.com.etecia.nutriapp.R;
 
 public class CadastroActivity extends AppCompatActivity {
 ImageView btnVoltarCadastro;
+Button btnCadastrar;
+TextInputEditText edtRegEmail, edtRegSenha;
+
+FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +41,52 @@ ImageView btnVoltarCadastro;
             return insets;
 
         });
+        edtRegSenha = findViewById(R.id.edtRegSenha);
+        edtRegEmail = findViewById(R.id.edtRegEmail);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
         btnVoltarCadastro = findViewById(R.id.btnVoltarCadastro);
+
+
         btnVoltarCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, senha;
+                email = String.valueOf(edtRegEmail.getText());
+                senha = String.valueOf(edtRegSenha.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(CadastroActivity.this, "Insira o E-mail!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(senha)){
+                    Toast.makeText(CadastroActivity.this, "Insira a Senha!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, senha)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(CadastroActivity.this, "Registrado!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(CadastroActivity.this, "Algo deu errado!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
--- app/src/main/java/br/com/etecia/nutriapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/etecia/nutriapp/activity/LoginActivity.java
@@ -2,20 +2,34 @@ package br.com.etecia.nutriapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.etecia.nutriapp.R;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnCadastrarLogin;
     Button btnLogin;
+    TextInputEditText edtEmail, edtSenha;
+
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +46,47 @@ public class LoginActivity extends AppCompatActivity {
         btnCadastrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
         btnLogin = findViewById(R.id.btnLogin);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
+                String email, senha;
+                email = String.valueOf(edtEmail.getText());
+                senha = String.valueOf(edtSenha.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Insira o E-mail!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(senha)){
+                    Toast.makeText(LoginActivity.this, "Insira a Senha!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email, senha)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this, "Logado!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Algo deu errado!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
